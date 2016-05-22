0184017472b0ec07dac7abf4893f015d99fec70e
Victor Temprano <tempranova@gmail.com>
2016-05-22 10:39:11 -0700

    Update to some small corrections and API fixes

--- api/index.php
+++ api/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 header('Content-type:application/json;charset=utf-8');
 
 // API creation
@@ -8,7 +8,7 @@ header('Content-type:application/json;charset=utf-8');
 
 // It will return coordinates and I will provide a key for all the "names", which can be languages, nations or treaties
 // Can return all by default, or it returns specific ones only asked for
-    
+
 // TO ADD
 // - Can add 'single vs multiple' parameter to allow getting polygons from different maps at once
 
@@ -21,9 +21,9 @@ if($mapsRequest) {
     $territories = false;
     $languages = false;
     $treaties = false;
-    
+
     $mapsRequestArray = explode(",", $mapsRequest);
-    
+
     foreach ($mapsRequestArray as $key => $value) {
         if($value=='languages') {
             $languageResults = return_results('http://www.native-land.ca/coordinates/indigenousLanguages.json',$nameRequest,$langRequest);
@@ -55,6 +55,10 @@ function return_results($fileURL,$nameRequest,$langRequest) {
             foreach ($thisJSON->features as $arr) {
                 foreach ($arr as $obj) {
                     if($obj->Slug==$value2) {
+                        // Get only first link for description (for language)
+                        $description_array = explode(",", $obj->description);
+                        $obj->description = $description_array[0];
+                        // Do french substitutions
                         if($langRequest=='fr') {
                             $obj->Name = $obj->FrenchName;
                             unset($obj->FrenchName);
@@ -77,18 +81,18 @@ function return_results($fileURL,$nameRequest,$langRequest) {
         foreach ($thisJSON->features as $arr) {
             foreach ($arr as $obj) {
                 if(isset($obj->Slug)) {
+                    // Get only first link for description (for language)
+                    $description_array = explode(",", $obj->description);
+                    $obj->description = $description_array[0];
+                    // If lang is french, substitute in those values
                     if($langRequest=='fr') {
                         $obj->Name = $obj->FrenchName;
+                        $obj->description = $obj->FrenchDescription;
                         unset($obj->FrenchName);
-                        if($fileURL=='http://www.native-land.ca/coordinates/indigenousTreaties.json') {
-                            $obj->description = $obj->FrenchDescription;
-                            unset($obj->FrenchDescription);
-                        }
+                        unset($obj->FrenchDescription);
                     } else {
                         unset($obj->FrenchName);
-                        if($fileURL=='http://www.native-land.ca/coordinates/indigenousTreaties.json') {
-                            unset($obj->FrenchDescription);
-                        }
+                        unset($obj->FrenchDescription);
                     }
                     array_push($result,$arr);
                 }
